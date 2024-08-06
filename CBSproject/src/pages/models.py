c65bf7ffb8c92ce120126d35f6acda2a2495959c
Juho Siitonen <juho_tuomas@msn.com>
2024-08-06 23:27:02 +0300

    added model for postings

--- CBSproject/src/pages/models.py
+++ CBSproject/src/pages/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+from django.contrib.auth.models import User
+
+class Posting(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	text = models.TextField()
+	likes = models.IntegerField(default=0)
