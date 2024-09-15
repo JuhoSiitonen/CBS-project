faf94324672347b79348d355601ba83f946dfb8d
Juho Siitonen <juho_tuomas@msn.com>
2024-09-15 16:59:02 +0300

    added first view

--- CBSproject/pages/views.py
+++ CBSproject/pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posting
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_posting_list = Posting.objects.order_by('likes')[:10]
+    context = {'latest_posting_list': latest_posting_list}
+    return render(request, 'pages/index.html', context)
