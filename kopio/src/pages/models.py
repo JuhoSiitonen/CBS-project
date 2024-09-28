e0183c76d9b468d0aca2b7fa60534cdcc53de152
Juho Siitonen <juho_tuomas@msn.com>
2024-09-29 00:34:37 +0300

    deleted old setup

--- kopio/src/pages/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from django.contrib.auth.models import User
-
-class Posting(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	text = models.TextField()
-	likes = models.IntegerField(default=0)
